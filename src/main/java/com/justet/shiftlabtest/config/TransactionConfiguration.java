9bdf5731cb1b0f6c830a62546eecf994b9720f94
Justet <dimfisl33t@gmail.com>
2026-04-01 18:48:52 +0700

    Добавлен конфиг для транзакций

---
+++ src/main/java/com/justet/shiftlabtest/config/TransactionConfiguration.java
@@ -0,0 +1,9 @@
+package com.justet.shiftlabtest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfiguration {
+}
